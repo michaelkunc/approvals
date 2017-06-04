d62e132e673403220869987c00a7bd38e7a06e0b
michaelkunc <mjkunc@gmail.com>
2017-06-04 16:20:04 -0700

    one more cosmetic change

--- api/views.py
+++ api/views.py
@@ -1,6 +1,4 @@
-# from django.shortcuts import render
 from rest_framework import generics
-# from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.renderers import JSONRenderer
 
