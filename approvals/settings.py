96fd1d9944cec8c95ddf4bfee8016293cd3aa1b7
michaelkunc <mjkunc@gmail.com>
2017-06-04 17:17:57 -0700

    heroku config

--- approvals/settings.py
+++ approvals/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import psycopg2
 import dj_database_url
+import urllib.parse as urlparse
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +125,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# heroku db config
+try:
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+
+    DATABASES['default'] = dj_database_url.config()
+
+# If DATABASE_URL is missing, use local db config.
+except KeyError:
+    DATABASES
